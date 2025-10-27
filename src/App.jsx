cb8fcbd2b3b7d2229cb8fa3f28c10a21e28cc33f
dumitrescuvlad <emiyazwei@gmail.com>
2025-10-27 18:00:34 +0100

    working with supabase following supabase docs : https://supabase.com/docs/guides/auth/quickstarts/react

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import supabase from "../supabaseClient";
+
+function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+  if (!session) {
+    return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+  } else {
+    return <div>Logged in!</div>;
+  }
+}
+
+export default App;
